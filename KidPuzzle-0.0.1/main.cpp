798757cff91be3ebaffb49a28e1e95cf1c50b275
Jakub Pavelek <jpavelek@live.com>
2012-02-04 20:57:56 +0100

    Changes: Got fullscreen, expanded is not good enough

--- KidPuzzle-0.0.1/main.cpp
+++ KidPuzzle-0.0.1/main.cpp
@@ -8,7 +8,8 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/KidPuzzle/main.qml"));
-    viewer.showExpanded();
+    //viewer.showExpanded();
+    viewer.showFullScreen();
 
     return app->exec();
 }
